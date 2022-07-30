ad6e740dea257e6c7d2668b5cfedd1f5c0de2477
wromo <info.wromo@gmail.com>
2022-07-30 21:05:09 +0200

    add62

---
+++ src/constants/constants.ts
@@ -0,0 +1,16 @@
+const CONSTANTS = {
+  purchaseUrl:
+    "https://wromox.wromo.com",
+  liveDemoUrl: "https://wromox.wromo.com.com/",
+  supportUrl: "https://wromox.wromo.com/",
+  documentationUrl: "https://wromox.wromo.com/",
+  socials: {
+    facebookUrl: "https://www.facebook.com/wromomerkant",
+    twitterUrl: "https://twitter.com/",
+    instagramUrl: "https://www.instagram.com/",
+    youtubeUrl: "https://www.youtube.com/",
+    githubUrl: "https://github.com/",
+  },
+};
+
+export { CONSTANTS };
